55e217ecf175875b219fdf582b050b7226873006
fors4ty <fors4ty@gmail.com>
2026-06-27 01:28:15 +0100

    Change MySQL connection to use a connection pool

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
